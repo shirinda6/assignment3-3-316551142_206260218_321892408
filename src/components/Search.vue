7e77b97c0ea43f4480e9e32d518a101f30972527
hallelhel <hallelh@post.bgu.ac.il>
2022-08-03 15:11:14 +0300

    serach component

---
+++ src/components/Search.vue
@@ -0,0 +1,196 @@
+<template>
+<div>
+<div  style="margin-left: 1%;">
+    <div>
+    <b-form-input style="width: 70%"
+    id="foodName"
+    placeholder="Enter food name"
+    v-model="foodName"></b-form-input>
+    </div>
+    <div class="col">
+  <b-button
+        type="submit"
+        variant="primary"
+        class=" w-15" @click="SearchRecipe"
+        ><b-icon icon="search" aria-hidden="true"></b-icon></b-button
+      >
+</div>
+</div>
+    <div>
+    <b-form-spinbutton
+      id="sb-step"
+      v-model="selected"
+      style="width:70% ;margin-top: 2%; padding: 1%;"
+      min="5"
+      max="15"
+      step="5"
+    ></b-form-spinbutton>
+  </div>
+    <div>
+  <b-dropdown id="filter" text="Filter" variant="primary" class="m-2" style="width: 70% !important; ">
+  <div role="tablist" class="accordion" style="min-height: 0 !important; overflow-y:auto;" >
+    <b-card no-body class="mb-1" style=" width: 300px !important;">
+      <b-card-header role="tab" class="p-1" header-tag="header">
+        <b-button block v-b-toggle.cuisines><b-icon icon="chevron-down" aria-hidden="true"></b-icon>  cuisines</b-button>
+      </b-card-header>
+      <b-collapse id="cuisines" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-form-checkbox v-for="cuisine in cuisines" 
+                v-bind:key="cuisine"
+                v-bind:value=cuisine
+                @input="insertCuisines(cuisine)">{{cuisine}}</b-form-checkbox>
+        </b-card-body>
+      </b-collapse>
+     </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header role="tab" class="p-1">
+        <b-button block v-b-toggle.diets><b-icon icon="chevron-down" aria-hidden="true"></b-icon>  diets</b-button>
+      </b-card-header>
+      <b-collapse id="diets"  visible role="tabpanel">
+        <b-card-body>
+          <b-form-checkbox v-for="diet in diets" 
+                            v-bind:key="diet"
+                            v-bind:value=diet
+                            @input="insertDiets(diet)">{{diet}}
+            </b-form-checkbox>
+        </b-card-body>
+      </b-collapse> 
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header role="tab" class="p-1">
+        <b-button block v-b-toggle.intolerances><b-icon icon="chevron-down" aria-hidden="true"></b-icon>  intolerances</b-button>
+      </b-card-header>
+      <b-collapse id="intolerances" visible role="tabpanel">
+        <b-card-body>
+          <b-form-checkbox v-for="intolerance in intolerances" 
+                    v-bind:key="intolerance"
+                    v-bind:value=intolerance
+                    @input="insertIntolerances(intolerance)">{{intolerance}}</b-form-checkbox>
+        </b-card-body>
+      </b-collapse>
+   </b-card>
+   </div>
+  </b-dropdown>
+</div>
+</div>
+</template>
+
+<script>
+import cuisines from "../assets/cuisines";
+  import diets from "../assets/diets";
+  import intolerances from "../assets/intolerances";
+
+export default{
+    data(){
+        return{
+            // foodName:"",
+             cuisines:[],
+        diets:[],
+        intolerances:[],
+        recipes:[]
+            // filtercuisines:[],
+            // filterdiets:[],
+            // filterintolerances:[],
+            // selected: 5,
+        }
+    },
+    props: {
+        selected:{
+            type: Number,
+            required: true
+        },
+        foodName:{
+            type: String,
+            required: true
+        },
+        filtercuisines:{
+            type: Array,
+            required: true
+        },
+        filterdiets:{
+            type: Array,
+            required: true
+        },
+        filterintolerances:{
+            type: Array,
+            required: true
+        },
+    },
+    mounted() {
+    this.cuisines.push(...cuisines);
+    this.diets.push(...diets);
+    this.intolerances.push(...intolerances);
+    },
+    methods:{
+    insertCuisines(item){
+      if (this.filtercuisines.includes(item)){
+        let index=this.filtercuisines.indexOf(item);
+        this.filtercuisines.splice(index,1);
+      }
+      else this.filtercuisines.push(item);
+      console.log(this.filtercuisines);
+    },
+    insertDiets(item){
+      if (this.filterdiets.includes(item)){
+        let index=this.filterdiets.indexOf(item);
+        this.filterdiets.splice(index,1);
+      }
+      else this.filterdiets.push(item);
+    },
+    insertIntolerances(item){
+      if (this.filterintolerances.includes(item)){
+        let index=this.filterintolerances.indexOf(item);
+        this.filterintolerances.splice(index,1);
+      }
+      else this.filterdiets.push(item);
+    },
+    async SearchRecipe(){
+      try {
+        let params={
+          numberOfRecipe:this.selected,
+          RecipeName:this.foodName,
+          cuisine:this.filtercuisines.join(","),
+          diet:this.filterdiets.join(","),
+          intolerance:this.filterintolerances.join(","),
+        }
+        this.axios.defaults.withCredentials = true;
+        const response = await this.axios.get(
+           this.$root.store.serverDomain+"/recipes/search",{params},{withCredentials: true}
+        );
+
+        console.log(response);
+        this.recipes=response.data.search;
+        
+        if(this.$root.store.username){
+          
+          const views= response.data.view;
+          const favorites= response.data.favorite;
+          
+          this.recipes.forEach((recipe)=>{
+            recipe.userView=views[recipe.id];
+            recipe.userFavorite=favorites[recipe.id];
+          });
+          let last={
+            paramsQuery:params,
+            resuilts:this.recipes
+          }
+          
+          this.$root.store.Search(last);
+        }
+        this.$emit('res',this.recipes);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.m-2 .dropdown-menu {
+    max-height: 300px;
+    overflow-y: auto;
+  }
+</style>
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,85 +1,13 @@
 <template>
   <div class="container" style="max-width: 1980px; width: 100%">
-    <h1 class="title">Search Page</h1>
-    <div style="width: 30%; float: left; margin-top: 3%;">
-    <div class="row row-col-2" style="margin-left: 1%;">
-    <div>
-    <b-form-input class="col-16"
-    id="foodName"
-    placeholder="Enter food name"
-    v-model="foodName"></b-form-input>
-    </div>
-    <div class="col">
-  <b-button
-        type="submit"
-        variant="primary"
-        class=" w-15" @click="SearchRecipe"
-        ><b-icon icon="search" aria-hidden="true"></b-icon></b-button
-      >
-</div>
-</div>
-    <div>
-    <b-form-spinbutton
-      id="sb-step"
-      v-model="selected"
-      style="width:70% ;margin-top: 2%; padding: 1%;"
-      min="5"
-      max="15"
-      step="5"
-    ></b-form-spinbutton>
-  </div>
-    <div>
-  <b-dropdown id="filter" text="Filter" variant="primary" class="m-2" style="width: 70% !important; ">
-  <div role="tablist" class="accordion" style="min-height: 0 !important; overflow-y:auto;" >
-    <b-card no-body class="mb-1" style=" width: 300px !important;">
-      <b-card-header role="tab" class="p-1" header-tag="header">
-        <b-button block v-b-toggle.cuisines><b-icon icon="chevron-down" aria-hidden="true"></b-icon>  cuisines</b-button>
-      </b-card-header>
-      <b-collapse id="cuisines" visible accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-form-checkbox v-for="cuisine in cuisines" 
-                v-bind:key="cuisine"
-                v-bind:value=cuisine
-                @input="insertCuisines(cuisine)">{{cuisine}}</b-form-checkbox>
-        </b-card-body>
-      </b-collapse>
-     </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header role="tab" class="p-1">
-        <b-button block v-b-toggle.diets><b-icon icon="chevron-down" aria-hidden="true"></b-icon>  diets</b-button>
-      </b-card-header>
-      <b-collapse id="diets"  visible role="tabpanel">
-        <b-card-body>
-          <b-form-checkbox v-for="diet in diets" 
-                            v-bind:key="diet"
-                            v-bind:value=diet
-                            @input="insertDiets(diet)">{{diet}}
-            </b-form-checkbox>
-        </b-card-body>
-      </b-collapse> 
-    </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header role="tab" class="p-1">
-        <b-button block v-b-toggle.intolerances><b-icon icon="chevron-down" aria-hidden="true"></b-icon>  intolerances</b-button>
-      </b-card-header>
-      <b-collapse id="intolerances" visible role="tabpanel">
-        <b-card-body>
-          <b-form-checkbox v-for="intolerance in intolerances" 
-                    v-bind:key="intolerance"
-                    v-bind:value=intolerance
-                    @input="insertIntolerances(intolerance)">{{intolerance}}</b-form-checkbox>
-        </b-card-body>
-      </b-collapse>
-   </b-card>
-   </div>
-  </b-dropdown>
-
-</div>
+    <h1 class="title" style="margin-left:45%; margin-top: 3%;">Search</h1>
+    <div v-if="flag">
+    <div style="width: 30%; float: left; margin-top: 4%;">
+    <Search1 @res="showResult" :selected="selected" :foodName="foodName" :filtercuisines="filtercuisines" :filterdiets="filterdiets" :filterintolerances="filterintolerances"/>
 </div>
 
 <div style="width: 70%; float: right;">
+
 <div style="margin-left: 65%; margin-bottom: 2%;">
   sort by:
   <input type="radio" v-model="sorted" value="readyInMinutes" @click="sortby('readyInMinutes')">preparation time
@@ -92,115 +20,57 @@
       </b-card-group>
     </b-row>
   </b-container>
+  
+  </div>
+  </div>
+
+  <div v-else style="margin-top: 4%;margin-left: 20%;">
+    <Search1 @res="showResult" :selected="selected" :foodName="foodName" :filtercuisines="filtercuisines" :filterdiets="filterdiets" :filterintolerances="filterintolerances"/>
   </div>
     </div>
 </template>
 
 <script>
-import cuisines from "../assets/cuisines";
-  import diets from "../assets/diets";
-  import intolerances from "../assets/intolerances";
+
   import RecipePreview from "../components/RecipePreview.vue";
+  import Search1 from "../components/Search.vue";
 
   export default{
     name: "Search",
     components: {
-    RecipePreview
+    RecipePreview,Search1
   },
     data(){
       return{
-        foodName:"",
-        cuisines: [],
-        diets: [],
-        intolerances: [],
-        allowFiltering : true,
-        filtercuisines:[],
-        filterdiets:"",
-        filterintolerances:"",
-        selected: '5',
         recipes: [],
         sorted:"",
         flag:false,
+        selected:5,
+        foodName:"",
+        filtercuisines:[],
+        filterdiets:[],
+        filterintolerances:[]
         }
   },
-    mounted() {
-    this.cuisines.push(...cuisines);
-    this.diets.push(...diets);
-    this.intolerances.push(...intolerances);
+    mounted() {console.log("mounted");
     if(this.$root.store.username && this.$root.store.lastSearch){
+      
       this.flag=true;
       this.recipes=this.$root.store.lastSearch.resuilts;
       let params=this.$root.store.lastSearch.paramsQuery;
       this.selected=params.numberOfRecipe;
       this.foodName=params.RecipeName;
       this.filtercuisines=params.cuisine.split(",");
-      this.filterdiets=params.diet;
-      this.filterintolerances=params.intolerance;
-      console.log(this.recipes);
+      this.filterdiets=params.diet.split(",");
+      this.filterintolerances=params.intolerance.split(",");
+      console.log("recipe",this.recipes);
     }
   },
   methods:{
-    insertCuisines(item){
-      if (this.filtercuisines.includes(item)){
-        let index=this.filtercuisines.indexOf(item);
-        this.filtercuisines.splice(index,1);
-      }
-      else this.filtercuisines.push(item);
-      // if (!this.filtercuisines){
-      //   this.filtercuisines=item;
-      // }
-      // else this.filtercuisines+=","+item;
-      console.log(this.filtercuisines);
-    },
-    insertDiets(item){
-      if (!this.filterdiets){
-        this.filterdiets=item;
-      }
-      else this.filterdiets+=","+item;
-    },
-    insertIntolerances(item){
-      if (!this.filterintolerances){
-        this.filterintolerances=item;
-      }
-      else this.filterintolerances+=","+item;
-    },
-    async SearchRecipe(){
-      try {
-        let params={
-          numberOfRecipe:this.selected,
-          RecipeName:this.foodName,
-          cuisine:this.filtercuisines.join(","),
-          diet:this.filterdiets,
-          intolerance:this.filterintolerances
-        }
-        this.axios.defaults.withCredentials = true;
-        const response = await this.axios.get(
-           this.$root.store.serverDomain+"/recipes/search",{params},{withCredentials: true}
-        );
-
-        console.log(response);
-        const recipes = response.data.search;
-        this.recipes = [];
-        this.recipes.push(...recipes);
+    
+     showResult(data){
+        this.recipes=data;
         this.flag=true;
-
-        if(this.$root.store.username){
-          const views= response.data.view;
-          const favorites= response.data.favorite;
-          this.recipes.forEach((recipe)=>{
-            recipe.userView=views[recipe.id];
-            recipe.userFavorite=favorites[recipe.id];
-          });
-          let last={
-            paramsQuery:params,
-            resuilts:this.recipes
-          }
-          this.$root.store.Search(last);
-        }
-        // console.log(this.recipes);
-      } catch (error) {
-        console.log(error);
-      }
     },
     sortby(field){
       if(field=="popularity")
